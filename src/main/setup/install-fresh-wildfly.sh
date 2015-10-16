d8f95d0da09ff3fc2d7073d213aca7d2513d468d
Miroslav Vasko <miroslav.vasko@cargo-partner.com>
2015-10-16 10:26:54 +0200

    Wildfly upgrade to 10CR2, simplified setup

---
+++ src/main/setup/install-fresh-wildfly.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+WILDFLY=wildfly-10.0.0.CR2
+PROJECT=~/Documents/projects/podcasts
+DOWNLOAD=~/Downloads/$WILDFLY.zip
+INSTALL=~/bin
+TARGET=wildfly
+
+cd $INSTALL
+test -d $TARGET && rm -rf $TARGET
+cp $DOWNLOAD ./
+unzip $WILDFLY.zip >/dev/null
+rm $WILDFLY.zip
+mv $WILDFLY $TARGET
+cd $TARGET/standalone/configuration
+rm standalone*.xml
+cp $PROJECT/src/main/setup/standalone-dev.xml ./standalone.xml
+ln -s standalone.xml standalone-full.xml
+ln -s standalone.xml standalone-full-ha.xml
+ln -s standalone.xml standalone-ha.xml
+cd ../../..
+
+./$TARGET/bin/add-user.sh admin admin --silent
